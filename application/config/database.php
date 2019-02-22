ce5c3a57bc0b9b69a260ea916e840d9e19816886
sumit <rounnn8@gmail.com>
2019-02-22 14:31:27 +0545

    gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
+
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
+	'database' => 'biling',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
